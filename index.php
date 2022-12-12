33ad2d8bd4d47e4b4b75551f7922fb99e0f7ca91
LuongoAndrea <andrea.luongo2002@gmail.com>
2022-12-12 21:46:16 +0100

    metodo e 2 film stampati a schermo

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 include_once __DIR__ . '/models/movie.php';
 $rick = new Movie("rick", "2016/12/26", "frenck", "/img/filmRick");
+$morti = new Movie("morti", "2016/12/26", "paolo", "/img/filmMorti");
+echo Movie::saluto();
 var_dump($rick);
+var_dump($morti);
+echo "<br>" . $morti->getGenere();
 ?>
--- models/movie.php
+++ models/movie.php
@@ -4,13 +4,25 @@ class Movie
     public $name;
     public $dataUsita;
     public $regista;
+    public static $genere = "fantasy";
     public $img;
+
     function __construct(string $_name, string $_dataUsita, string $_regista, string $_img)
     {
         $this->name = $_name;
         $this->dataUsita = $_dataUsita;
         $this->regista = $_regista;
+
         $this->img = $_img;
     }
+    public static function saluto()
+    {
+        return "ciao guarda i migliori film";
+    }
+    public function getGenere()
+    {
+        return "il genere dei clienti è: " . self::$genere;
+    }
+
 }
 ?>
